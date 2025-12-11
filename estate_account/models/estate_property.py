48460628cd29f694944f1ec31e8f3b8cec7c7716
Jose Serrano Quesada <joseserranoq00@gmail.com>
2025-12-10 20:24:41 -0600

    Chapter 13 finished

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -3,8 +3,26 @@ from odoo import models, fields, exceptions
 class estateProperty(models.Model):
     _name = 'estate.property'
     _inherit = 'estate.property'
-
+#Invoice creation
     def action_sold(self): 
         res = super(estateProperty, self).action_sold()
-        print('Response: ',res)
+        
+        # Create invoice with two lines
+        self.env['account.move'].create({
+            'partner_id': self.buyer_id.id,
+            'move_type': 'out_invoice',
+            'invoice_line_ids': [
+                (0, 0, {
+                    'name': 'Commission (6%)',
+                    'quantity': 1,
+                    'price_unit': self.selling_price * 0.06,
+                }),
+                (0, 0, {
+                    'name': 'Administrative Fees',
+                    'quantity': 1,
+                    'price_unit': 100.0,
+                }),
+            ],
+        })
+        
         return res
